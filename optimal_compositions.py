5dabc4d533fca7362f93bab3edb442429c21fb4a
IdanSeg <idansagel@gmail.com>
2025-01-17 22:07:24 +0200

    update optimal_compositions.py to compare to pbmc_sick

--- optimal_compositions.py
+++ optimal_compositions.py
@@ -65,6 +65,18 @@ if dataset_name == 'pbmc_healthy':
     percentile = 90
     batch_size = 64
 
+if dataset_name == 'pbmc_sick':
+    dataset = PBMC()
+    dataset.load_data()
+    adata = dataset.preprocess_data()
+    adata = dataset.filter_by_health(clear_sick=False)
+    label_key = 'cell_type'
+    epoch_num_annot = 50
+    epoch_num_composition = 25
+    swap_probability = 0.1
+    percentile = 90
+    batch_size = 64
+
 format_manager = AnnDataManager()
 
 format_manager.general_info(adata)
@@ -73,4 +85,8 @@ best_compositions, label_encoder = find_optimal_compositions(dataset_name, adata
                         repeats_per_size, device, epoch_num_composition, batch_size)
 visualize_optimal_compositions(dataset_name)
 highest_confidence_samples(adata, train_sizes, device, label_encoder, dataset_name, label_key)
-comp_opt_subset_to_not(dataset_name, adata, label_key, group_counts, device, epoch_num_composition, epoch_num_annot, batch_size, format_manager)
+for T in train_sizes:
+    E, A, H = best_compositions[T]['composition']
+    group_counts = {'Easy-to-learn': E, 'Ambiguous': A, 'Hard-to-learn': H}
+    comp_opt_subset_to_not(dataset_name, adata, label_key, group_counts, device, epoch_num_composition, epoch_num_annot, batch_size, format_manager)
+    comp_opt_subset_to_not('pbmc_sick', adata, label_key, group_counts, device, epoch_num_composition, epoch_num_annot, batch_size, format_manager)
